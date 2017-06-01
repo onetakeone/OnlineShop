dab81fee417e5b0ea9229fbbfeaa0567d793792f
N <limepassion@gmail.com>
2017-06-01 11:25:07 +0300

    +migration

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:onlineshop.db3"
+
+class Showcase < ActiveRecord::Base
+end
 
 get '/' do
 	erb :index
---
+++ db/migrate/20170601081202_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+  	create_table :products do |p|
+  		p.text :title
+  		p.text :desc
+  		p.decimal :price
+  		p.decimal :netto
+  		p.boolean :is_breakfast
+  		p.boolean :is_best_offer
+  		p.string :path_to_image 
+  		p.timestamps
+  	end
+ 	end
+end
